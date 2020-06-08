533da86d37a5cd4d7406c171924331e34065d187
Christopher D. Philipson <61236782+PhilipsonChristopher@users.noreply.github.com>
2020-06-08 13:03:27 +0200

    Update Figure1Code.R

--- Fig1/Figure1Code.R
+++ Fig1/Figure1Code.R
@@ -47,7 +47,7 @@ str(Logged)
 
 m1 <- lmer(ACD~  YearsSinceLogging  * FACE + (1|Plot)  + (1|LoggingMethod:Coupe) , REML=1 , data=Logged , na.action=na.fail) 
 dotplot(ranef(m1, condVar=T))$Plot
-dotplot(ranef(m1, condVar=T))$'LoggingMethod:Coupe'
+dotplot(ranef(m1, condVar=T))$'LoggingMethod:Coupe'
 anova(m1)
 summary(m1)
 confint(m1)
